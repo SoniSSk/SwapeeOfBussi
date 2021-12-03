92b60db57c30be15f74ea5e5fa32365c6cdb28d0
SoniSSk <kuldeepsoni3099@gmail.com>
2021-12-03 17:51:06 +0530

    aaA

--- src/components/header/NavigationItem.js
+++ src/components/header/NavigationItem.js
@@ -7,17 +7,16 @@ import PrSVG from '../commons/svg/PrSVG';
 import ProjectSVG from '../commons/svg/ProjectSVG';
 import InsightsSVG from '../commons/svg/InsightsSVG';
 
-
 const NavigationItemWrapper = styled.div`
-    
-    border-radius: 3px 3px 3px 3px;
-    border-bottom: 3px solid transparent;
+    border: 1px solid transparent;
+    border-radius: 3px 3px 0 0;
+    border-top: 3px solid transparent;
     color: ${props => (props.selected ? '#24292e' : '#586069')};
     float: left;
     padding: 7px 15px 8px;
     white-space: nowrap;
-    background-color : ${props => props.selected && '24292e'};
-    border-color : ${props => props.selected && '#e36209  transparent'};
+    background-color : ${props => props.selected && '#fff'};
+    border-color : ${props => props.selected && '#e36209 #e1e4e8 transparent'};
     cursor : pointer;
     &:hover {
      color : #000;
@@ -50,9 +49,9 @@ const NavigationItem = ({
     {name === 'Pull Requests' && <PrSVG />}
     {name === 'Projects' && <ProjectSVG />}
     {name === 'Insights' && <InsightsSVG />}
-    {name === 'Wiki' && <IssueOpenedSVG  />}
-    {name === 'Security' && <IssueOpenedSVG  />}
-    {name === 'Action' && <IssueOpenedSVG  />}
+    {name === 'Wiki' &&  <IssueOpenedSVG />}
+    {name === 'Security' &&  <IssueOpenedSVG />}
+    {name === 'Action' && <IssueOpenedSVG />}
     <NavigationItemAnchor href={url}>{name}</NavigationItemAnchor>
     { value
       ? <NavigationItemValue>{value}</NavigationItemValue>
