b24443adac51a1a6b55f34970b69fba9be522467
SoniSSk <kuldeepsoni3099@gmail.com>
2021-12-03 18:09:45 +0530

    dddd

--- src/components/header/Footer.js
+++ src/components/header/Footer.js
@@ -50,6 +50,8 @@ const Footer = ({
   forks_count,
 }) => (
   <Nav>
+     <p><strong>ProTip</strong> What's not been update in a month:<a src="#">update:2021-08-17</a></p>
+     <hr/>
     <FooterMenu >
     {
         FooterValve.map(item => (
